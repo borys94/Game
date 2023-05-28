39544c6a07fa43fe68d3c43e2f239ab07cc2fa1d
borys94 <mkudla94@o2.pl>
2023-05-28 21:05:56 +0200

    add readme

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,5 +1,5 @@
-export const CANVAS_WIDTH = 576 * 1.5
-export const CANVAS_HEIGHT = 324 * 1.5
+export const CANVAS_WIDTH = 576 * 1.2
+export const CANVAS_HEIGHT = 324 * 1.2
 export const TILE_SIZE = 32 // dont modify!!
 
 const config = {
